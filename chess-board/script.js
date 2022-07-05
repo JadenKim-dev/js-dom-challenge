d7531070aac0e54ed0de68a3b29d1200cdc0dff5
JadenKim-dev <jaden.kim5179@gmail.com>
2022-07-05 09:33:58 +0900

    challenge 3 : chess-board

---
+++ chess-board/script.js
@@ -0,0 +1,60 @@
+const BLACK_COL_CLASSNAME = 'col black_col';
+const WHITE_COL_CLASSNAME = 'col white_col';
+const RED_COL_CLASSNAME = 'col red_col';
+
+const dr = [-1, 1, -1, 1];
+const dc = [-1, -1, 1, 1];
+
+const isSumOdd = (n1, n2) => (n1 + n2) % 2 == 1;
+
+function ChessBoard(el, rows, cols) {
+	let axis = [-1, -1];
+	const resultElement = document.querySelector(el);
+	
+	const setAxis = (r, c) => {
+		const [rr, cc] = axis;
+		paint(rr, cc, isSumOdd(rr, cc) ? 'b' : 'w');
+		paint(r, c, 'r');
+		axis = [r, c];
+	};
+	
+	const paint = (r, c, code) => {
+		const newClassName =
+			code === 'w' ? WHITE_COL_CLASSNAME : code === 'b' ? BLACK_COL_CLASSNAME : RED_COL_CLASSNAME;
+		if (r == -1 && c == -1) return;
+		for (let i = 0; i <= Math.max(rows, cols); i++) {
+			for (let j = 0; j < 4; j++) {
+				const nr = r + dr[j] * i;
+				const nc = c + dc[j] * i;
+				if (nr < 0 || nr >= rows || nc < 0 || nc >= cols) continue;
+				resultElement.children[nr].children[nc].className = newClassName;
+				if (i == 0) break;
+			}
+		}
+	};
+
+	const fragment = document.createDocumentFragment();
+
+	for (let r = 0; r < rows; r++) {
+		const rowDiv = document.createElement('div');
+		rowDiv.className = 'row';
+		for (let c = 0; c < cols; c++) {
+			const colDiv = document.createElement('div');
+			colDiv.dataset.row = r;
+			colDiv.dataset.col = c;
+			if (isSumOdd(r, c)) colDiv.className = BLACK_COL_CLASSNAME;
+			else {
+				colDiv.className = WHITE_COL_CLASSNAME;
+			}
+			rowDiv.appendChild(colDiv);
+		}
+		fragment.appendChild(rowDiv);
+	}
+	resultElement.appendChild(fragment);
+
+	resultElement.addEventListener('click', (e) => {
+		let { row, col } = e.target.dataset;
+		if (!row || !col) return;
+		setAxis(parseInt(row), parseInt(col));
+	});
+}
